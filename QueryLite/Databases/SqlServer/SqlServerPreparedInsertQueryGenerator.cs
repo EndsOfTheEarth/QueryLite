6ebf5a6b4041fbde27d268be5877f77b306a63da
EndsOfTheEarth <N/A>
2023-06-03 12:53:44 +1200

    Changes so far

---
+++ QueryLite/Databases/SqlServer/SqlServerPreparedInsertQueryGenerator.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * MIT License
+ *
+ * Copyright (c) 2023 EndsOfTheEarth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **/
+using QueryLite.Databases.SqlServer.Collectors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryLite.Databases.SqlServer {
+
+    internal sealed class SqlServerPreparedInsertQueryGenerator {
+
+        public string GetSql<PARAMETERS, RESULT>(PreparedInsertTemplate<PARAMETERS> template, IDatabase database, out List<ISetParameter<PARAMETERS>> parameters, Func<IResultRow, RESULT>? outputFunc) {
+
+            StringBuilder sql = StringBuilderCache.Acquire(capacity: 256);
+
+            sql.Append("INSERT INTO ");
+
+            string schemaName = database.SchemaMap(template.Table.SchemaName);
+
+            if(!string.IsNullOrWhiteSpace(schemaName)) {
+                SqlServerHelper.AppendEnclose(sql, schemaName, forceEnclose: false);
+                sql.Append('.');
+            }
+
+            SqlServerHelper.AppendEnclose(sql, template.Table.TableName, forceEnclose: template.Table.Enclose);
+
+            StringBuilder paramSql = StringBuilderCache.Acquire();
+
+            PreparedSetValuesCollector<PARAMETERS> valuesCollector = new PreparedSetValuesCollector<PARAMETERS>(sql, paramSql: paramSql, database, CollectorMode.Insert);
+
+            sql.Append('(');
+
+            template.SetValues!(valuesCollector); //Note: This outputs sql to the sql string builder
+
+            sql.Append(')');
+
+            parameters = valuesCollector.InsertParameters;
+
+            GetReturningSyntax(template, sql, outputFunc);
+
+            sql.Append(" VALUES(").Append(paramSql).Append(')');
+
+            StringBuilderCache.Release(paramSql);
+
+            return StringBuilderCache.ToStringAndRelease(sql);
+        }
+
+        private static void GetReturningSyntax<PARAMETERS, RESULT>(PreparedInsertTemplate<PARAMETERS> template, StringBuilder sql, Func<IResultRow, RESULT>? outputFunc) {
+
+            if(outputFunc != null) {
+
+                SqlServerReturningFieldCollector collector = SqlServerReturningCollectorCache.Acquire(isDelete: false, sql);
+
+                sql.Append(" OUTPUT ");
+
+                outputFunc(collector);
+
+                SqlServerReturningCollectorCache.Release(collector);
+            }
+        }
+    }
+}
