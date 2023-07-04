ebc032c5818fce2b98e5bb39ecd317a23479d29f
EndsOfTheEarth <N/A>
2023-07-05 06:55:48 +1200

    Add comments to IResultRow

--- QueryLite/Result/IResultRow.cs
+++ QueryLite/Result/IResultRow.cs
@@ -29,138 +29,270 @@ namespace QueryLite {
 
         internal void Reset();
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public string Get(Column<string> column);
         public string? Get(NullableColumn<string> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public Guid Get(Column<Guid> column);
         public Guid? Get(NullableColumn<Guid> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public bool Get(Column<bool> column);
         public bool? Get(NullableColumn<bool> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public Bit Get(Column<Bit> column);
         public Bit? Get(NullableColumn<Bit> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public decimal Get(Column<decimal> column);
         public decimal? Get(NullableColumn<decimal> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public short Get(Column<short> column);
         public short? Get(NullableColumn<short> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public int Get(Column<int> column);
         public int? Get(NullableColumn<int> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public long Get(Column<long> column);
         public long? Get(NullableColumn<long> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public float Get(Column<float> column);
         public float? Get(NullableColumn<float> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public double Get(Column<double> column);
         public double? Get(NullableColumn<double> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public TimeOnly Get(Column<TimeOnly> column);
         public TimeOnly? Get(NullableColumn<TimeOnly> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public DateTime Get(Column<DateTime> column);
         public DateTime? Get(NullableColumn<DateTime> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public DateOnly Get(Column<DateOnly> column);
         public DateOnly? Get(NullableColumn<DateOnly> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public DateTimeOffset Get(Column<DateTimeOffset> column);
         public DateTimeOffset? Get(NullableColumn<DateTimeOffset> column);
 
         public byte Get(Column<byte> column);
         public byte? Get(NullableColumn<byte> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public byte[] Get(Column<byte[]> column);
         public byte[]? Get(NullableColumn<byte[]> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public ENUM GetEnum<ENUM>(Column<ENUM> column) where ENUM : notnull, Enum;
         public ENUM? GetEnum<ENUM>(NullableColumn<ENUM> column) where ENUM : notnull, Enum;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public StringKey<TYPE> Get<TYPE>(Column<StringKey<TYPE>> column) where TYPE : notnull;
         public StringKey<TYPE>? Get<TYPE>(NullableColumn<StringKey<TYPE>> column) where TYPE : notnull;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public GuidKey<TYPE> Get<TYPE>(Column<GuidKey<TYPE>> column) where TYPE : notnull;
         public GuidKey<TYPE>? Get<TYPE>(NullableColumn<GuidKey<TYPE>> column) where TYPE : notnull;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public ShortKey<TYPE> Get<TYPE>(Column<ShortKey<TYPE>> column) where TYPE : notnull;
         public ShortKey<TYPE>? Get<TYPE>(NullableColumn<ShortKey<TYPE>> column) where TYPE : notnull;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public IntKey<TYPE> Get<TYPE>(Column<IntKey<TYPE>> column) where TYPE : notnull;
         public IntKey<TYPE>? Get<TYPE>(NullableColumn<IntKey<TYPE>> column) where TYPE : notnull;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public LongKey<TYPE> Get<TYPE>(Column<LongKey<TYPE>> column) where TYPE : notnull;
         public LongKey<TYPE>? Get<TYPE>(NullableColumn<LongKey<TYPE>> column) where TYPE : notnull;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public BoolValue<TYPE> Get<TYPE>(Column<BoolValue<TYPE>> column) where TYPE : notnull;
         public BoolValue<TYPE>? Get<TYPE>(NullableColumn<BoolValue<TYPE>> column) where TYPE : notnull;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public string Get(Function<string> column);
         public string? Get(NullableFunction<string> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public Guid Get(Function<Guid> column);
         public Guid? Get(NullableFunction<Guid> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public bool Get(Function<bool> column);
         public bool? Get(NullableFunction<bool> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public Bit Get(Function<Bit> column);
         public Bit? Get(NullableFunction<Bit> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public short Get(Function<short> column);
         public short? Get(NullableFunction<short> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public int Get(Function<int> column);
         public int? Get(NullableFunction<int> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public long Get(Function<long> column);
         public long? Get(NullableFunction<long> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public float Get(Function<float> column);
         public float? Get(NullableFunction<float> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public double Get(Function<double> column);
         public double? Get(NullableFunction<double> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public DateTime Get(Function<DateTime> column);
         public DateTime? Get(NullableFunction<DateTime> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public DateTimeOffset Get(Function<DateTimeOffset> column);
         public DateTimeOffset? Get(NullableFunction<DateTimeOffset> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public DateOnly Get(Function<DateOnly> column);
         public DateOnly? Get(NullableFunction<DateOnly> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public TimeOnly Get(Function<TimeOnly> column);
         public TimeOnly? Get(NullableFunction<TimeOnly> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public byte Get(Function<byte> column);
         public byte? Get(NullableFunction<byte> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public byte[] Get(Function<byte[]> column);
         public byte[]? Get(NullableFunction<byte[]> column);
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public ENUM GetEnum<ENUM>(Function<ENUM> column) where ENUM : notnull, Enum;
         public ENUM? GetEnum<ENUM>(NullableFunction<ENUM> column) where ENUM : notnull, Enum;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public StringKey<TYPE> Get<TYPE>(Function<StringKey<TYPE>> column) where TYPE : notnull;
         public StringKey<TYPE>? Get<TYPE>(NullableFunction<StringKey<TYPE>> column) where TYPE : notnull;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public GuidKey<TYPE> Get<TYPE>(Function<GuidKey<TYPE>> column) where TYPE : notnull;
         public GuidKey<TYPE>? Get<TYPE>(NullableFunction<GuidKey<TYPE>> column) where TYPE : notnull;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public ShortKey<TYPE> Get<TYPE>(Function<ShortKey<TYPE>> column) where TYPE : notnull;
         public ShortKey<TYPE>? Get<TYPE>(NullableFunction<ShortKey<TYPE>> column) where TYPE : notnull;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public IntKey<TYPE> Get<TYPE>(Function<IntKey<TYPE>> column) where TYPE : notnull;
         public IntKey<TYPE>? Get<TYPE>(NullableFunction<IntKey<TYPE>> column) where TYPE : notnull;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public LongKey<TYPE> Get<TYPE>(Function<LongKey<TYPE>> column) where TYPE : notnull;
         public LongKey<TYPE>? Get<TYPE>(NullableFunction<LongKey<TYPE>> column) where TYPE : notnull;
 
+        /// <summary>
+        /// Returns the selected value if it exists otherwise the default value for the type.
+        /// </summary>
         public BoolValue<TYPE> Get<TYPE>(Function<BoolValue<TYPE>> column) where TYPE : notnull;
         public BoolValue<TYPE>? Get<TYPE>(NullableFunction<BoolValue<TYPE>> column) where TYPE : notnull;
     }
